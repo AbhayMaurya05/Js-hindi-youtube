2823230f83aa669d731e3ab9fc365fdca9e1e71c
AbhayMaurya05 <140651573+AbhayMaurya05@users.noreply.github.com>
2025-07-04 04:35:07 +0000

    datatype-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 345415215215651n
+
+
+
+// Refrence (Non-Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj ={
+    name: "Abhay",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hello World");
+}
+
+console.log(typeof bigNumber);
